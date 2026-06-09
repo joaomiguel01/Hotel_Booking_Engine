2be862462d12d56486e074b7e91d1dc3c773e50f
João Miguel <jmocarvallho7123@gmail.com>
2026-06-09 12:32:38 -0300

    feature: Settings - Part 3

--- public/admin/inc/essentials.php
+++ public/admin/inc/essentials.php
@@ -1,5 +1,14 @@
 <?php 
 
+    // frontend puporse data
+    define('SITE_URL', 'http://localhost:8000/');
+    define('ABOUT_IMG_PATH', SITE_URL.'images/about/');
+
+
+    // backend upload process needs this data
+    define('UPLOAD_IMAGE_PATH', $_SERVER['DOCUMENT_ROOT'].'/images/');
+    define('ABOUT_FOLDER', 'about/');
+
     function adminLogin(){
         session_start();
         if(!(isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){
@@ -21,6 +30,39 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         alert;
+    }
+
+    function uploadImage($image, $folder){
+        $valid_mime = ['image/jpeg', 'image/png', 'image/webp', 'image/jpg'];
+        $img_mime = $image['type'];
+
+        if(!(in_array($img_mime, $valid_mime))){
+            return 'inv_img'; // invalid image mime or format
+        }
+        else if ($image['size']/(1024*1024) > 2){
+            return 'inv_size'; // invalid size greater than 2mb
+        }
+        else{
+            $ext = pathinfo($image['name'], PATHINFO_EXTENSION);
+            $rname = 'IMG_'.random_int(11111, 99999)."$ext";
+            $img_path = UPLOAD_IMAGE_PATH.$folder.$rname;
+
+            if(move_uploaded_file($image['tmp_name'], $img_path)){
+                return $rname;
+            }
+            else{
+                return 'upd_failed';
+            }
+        }
+    }
+
+    function deleteImage($image, $folder){
+        if(unlink(UPLOAD_IMAGE_PATH.$folder.$image)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 ?>
